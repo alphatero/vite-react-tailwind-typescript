2c28f29e145b8116359a1eac9f8e0e1160abfa60
Alpha <alphatero@icloud.com>
2021-08-01 23:15:59 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,64 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import './css/app.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+    <div className="App container-xl p-6 md:w-full">
+      <div className="flex md:flex-row flex-col 
+      items-center h-screen justify-center 
+      text-white my-10 md:my-0">
+        <div className="md:flex-1 w-full md:pb-0 pb-10">
+          <h1 className="text-3xl font-bold "
+          >Learn to code by<br /> watching others</h1>
+          <p className="text-center my-4">See how experienced developers solve problems in real-time. 
+            Watching scripted tutorials is great, 
+            but understanding how developers think is invaluable. </p>
+        </div>
+        <div className="md:flex-1 max-w-screen-sm md:px-11 px-0 flex
+        flex-col">
+          <button 
+          className="btn  
+          text-white text-base bg-purple rounded-lg
+          shadow-sp py-5 px-5 my-6
+          w-full 
+          "
+          ><span className="font-bold">Try it free 7 days </span> 
+          then $20/mo. thereafter</button>
+
+          <form action="" 
+          className="rounded-lg bg-white
+          shadow-sp w-full p-6">
+            <input type="text" 
+            id="firstName" placeholder="First Name"
+            className="border-gray border-solid border text-black 
+            py-3 focus:border-purple px-8 mb-5 w-full"/>
+            <input type="text" 
+            id="lastName" placeholder="Last Name"
+            className="border-gray border-solid border text-black 
+            py-3 focus:border-purple px-8 mb-5 w-full"/>
+            <input type="mail" 
+            id="eMail" placeholder="Email Address"
+            className="border-gray border-solid border text-black 
+            py-3 focus:border-purple px-8 mb-5 w-full"/>
+            <input type="password" 
+            id="password" placeholder="Password"
+            className="border-gray border-solid border text-black 
+            py-3 focus:border-purple px-8 mb-5 w-full"/>
+
+            <button className="btn bg-green rounded-lg
+          shadow-green w-full
+          text-lg py-4 mb-2">CLAIM YOUR FREE TRIAL</button>
+          <p className="text-gray text-xs">By clicking the button, you are agreeing to our 
+          <span className="text-primary"> Terms and Services</span></p>
+          </form>
+        </div>
+      </div>
+
+      {/* <header className="App-header">
+        <p>Learn to code by watching others</p>
         <p>
           <button onClick={() => setCount((count) => count + 1)}>
             count is: {count}
@@ -37,7 +86,7 @@ function App() {
             Vite Docs
           </a>
         </p>
-      </header>
+      </header> */}
     </div>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      purple: "#5E54A4",
+      white: "#ffffff",
+      black: "#3D3B48",
+      primary: "#FF7979",
+      green: "#77E2B3",
+      gray: "#DEDEDE",
+    },
+    boxShadow: {
+      sp: '0 8px 0 0 rgba(0, 0, 0, 0.1469)',
+      green: '0 4px 0 0 #38CC8B'
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
